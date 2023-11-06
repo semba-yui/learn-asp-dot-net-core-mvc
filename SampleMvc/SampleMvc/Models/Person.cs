b83b933670a1cf76a9f1229ecb027dab9932a997
Ryuichiro Semba <65758369+semba-yui@users.noreply.github.com>
2023-11-06 18:23:40 +0900

    feat: 3.3.10: 拡張Aタグの利用

--- SampleMvc/SampleMvc/Models/Person.cs
+++ SampleMvc/SampleMvc/Models/Person.cs
@@ -1,10 +1,15 @@
-﻿namespace SampleMvc.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleMvc.Models;
 
 public partial class Person
 {
     public int Id { get; set; }
 
+    [Display(Name = "名前")]
     public string Name { get; set; } = null!;
 
+    [Display(Name = "年齢")]
+    [DisplayFormat(DataFormatString = "{0} 歳")]
     public int Age { get; set; }
 }
