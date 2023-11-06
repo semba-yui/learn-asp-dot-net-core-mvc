73e8e382700c16beeb2cbff75b5c412ffaf06c5a
Ryuichiro Semba <65758369+semba-yui@users.noreply.github.com>
2023-11-06 17:02:18 +0900

    feat: add CRUD Controller

--- SampleMvc/SampleMvc/Program.cs
+++ SampleMvc/SampleMvc/Program.cs
@@ -1,7 +1,11 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using SampleMvc.Models;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<MvcdbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
