03b6b98852e9bcf34d212d18623630a82b109b18
Ryuichiro Semba <65758369+semba-yui@users.noreply.github.com>
2023-11-07 19:31:53 +0900

    feat: 3.5: バリデーションを追加

--- SampleMvc/SampleMvc/Models/Person.cs
+++ SampleMvc/SampleMvc/Models/Person.cs
@@ -7,9 +7,11 @@ public partial class Person
     public int Id { get; set; }
 
     [Display(Name = "名前")]
+    [MaxLength(10, ErrorMessage ="名前は10文字以内でお願いします")]
     public string Name { get; set; } = null!;
 
     [Display(Name = "年齢")]
     [DisplayFormat(DataFormatString = "{0} 歳")]
+    [Range(18, 100, ErrorMessage ="年齢は18歳から100歳までです")]
     public int Age { get; set; }
 }
